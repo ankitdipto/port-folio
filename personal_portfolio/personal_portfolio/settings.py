12d38c821bc7cd5618a0d49b057fe9e81e4ba29b
ankitdipto <ankitdipto@gmail.com>
2019-11-02 15:50:10 +0530

    changed allowed hosts

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0fy0-ho79%du%a2h05i5xqkhkrkshvtsjsastb*=u#a+vl-19h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
